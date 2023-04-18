e8c88074f6e057a5b5156ac7ddfc34b5735241e1
Erick <erick7451@gmail.com>
2023-04-18 13:34:29 -0500

    updated some steps towards installation

--- requirements/original_installation_env.sh
+++ requirements/original_installation_env.sh
@@ -1,6 +1,6 @@
 #ls /usr/local | grep cuda # cuda cuda-11.0
 #nvidia A100 (ampere architecture)
-conda create -n mmtrack python=3.7 y
+conda create -n mmtrack python=3.7 -y
 conda activate mmtrack
 
 pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 -f https://download.pytorch.org/whl/torch_stable.html
@@ -9,13 +9,12 @@ pip install -U openmim
 mim install mmengine
 mim install "mmcv-full<1.7.0" # 1.6.2
 mim install "mmdet<3.0.0"
-mim install "mmcv<2.0.0"
-python -c "import mmdet, mmcv; print(mmdet.__version__, mmcv.__version__)" # 1.6.2 2.28.2
+python -c "import mmdet, mmcv; print(mmdet.__version__, mmcv.__version__)" # 2.28.2 1.6.2
 git clone https://github.com/eplatero97/DeepFASORT
 cd DeepFASORT
 pip install -r requirements/build.txt
 pip install -v -e .
 pip install git+https://github.com/JonathonLuiten/TrackEval.git
-python -c "import mmdet, mmcv, mmcls; print(mmdet.__version__, mmcv.__version__, mmcls.__version__)" # 1.6.2 2.28.2 0.25.0
-pip install -U numpy # 1.24.2
+python -c "import mmdet, mmcv, mmcls; print(mmdet.__version__, mmcv.__version__, mmcls.__version__)" # 2.28.2 1.6.2 0.25.0
+pip install -U numpy # 1.21.6
 python demo/demo_mot_vis.py configs/mot/deepsort/sort_faster-rcnn_fpn_4e_mot17-private.py --input demo/demo.mp4 --output mot.mp4
